282e704049e9cd8d42babffbb854421b48718698
quoth-the-dev <andrewgorman.dev@gmail.com>
2023-12-24 10:54:51 +0100

    day2p1

--- day2/1.py
+++ day2/1.py
@@ -1,17 +1,36 @@
 import os
+import re
 
 import helpers
 
 
 def get_possible_games(file_path: os.path):
     possible_games = _get_possible_games(file=file_path)
-    pass
+    return sum(possible_games)
 
 
 def _get_possible_games(file: os.path):
     with open(file) as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        lines = puzzle_input.readlines()
+        possible = []
+        for line in lines:
+            game_number = int(re.findall(r"Game \d+", line.strip())[0].split()[1])
+            cubes = line.strip().split(":")[1]
+            red = re.findall(r"\d+ red", cubes)
+            green = re.findall(r"\d+ green", cubes)
+            blue = re.findall(r"\d+ blue", cubes)
+            impossible_red = _is_game_impossible(colour=red, max_for_colour=12)
+            impossible_green = _is_game_impossible(colour=green, max_for_colour=13)
+            impossible_blue = _is_game_impossible(colour=blue, max_for_colour=14)
+            if any([impossible_red, impossible_green, impossible_blue]):
+                continue
+            else:
+                possible.append(game_number)
+        return possible
+
+
+def _is_game_impossible(colour: list[str], max_for_colour: int):
+    return any(int(entry.split()[0]) > max_for_colour for entry in colour)
 
 
 helpers.print_timed_results(day=2, solution_func=get_possible_games)
