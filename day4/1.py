f5f94eae34eb6208f65f41b716dc3ed7c4634fdb
andrew gorman <andrew.gorman@hydrogrid.eu>
2023-12-25 16:37:24 +0100

    day4p1 and p2

--- day4/1.py
+++ day4/1.py
@@ -1,17 +1,25 @@
 import os
+import re
 
 import helpers
 
 
-def RENAME_FUNC(file_path: os.path):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def get_scratchcard_points(file_path: os.path):
+    return _get_scratchcard_points(file=file_path)
 
 
-def _RENAME_FUNC(file: os.path):
+def _get_scratchcard_points(file: os.path) -> int:
     with open(file) as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        cards = puzzle_input.readlines()
+        score = 0
+        for card in cards:
+            pipe_card = card.replace(":", "|")
+            winning_numbers = set(map(int, re.findall(r"\d+", pipe_card.split("|")[1])))
+            card_numbers = set(map(int, re.findall(r"\d+", pipe_card.split("|")[2])))
+            matches = len(winning_numbers.intersection(card_numbers))
+            points = 2 ** (matches - 1) if matches != 0 else 0
+            score += points
+        return score
 
 
-helpers.print_timed_results(day=1, solution_func=RENAME_FUNC)
+helpers.print_timed_results(day=1, solution_func=get_scratchcard_points)
--- day4/2.py
+++ day4/2.py
@@ -1,17 +1,30 @@
 import os
-
+import re
 import helpers
 
 
-def RENAME_FUNC(file_path: os.path):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def get_total_scratchcards(file_path: os.path):
+    all_cards_won = _get_scratchcard_copies(file=file_path)
+    total = 0
+    for card, copies in all_cards_won.items():
+        total += copies
+    return total
 
 
-def _RENAME_FUNC(file: os.path):
+def _get_scratchcard_copies(file: os.path):
     with open(file) as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        cards = puzzle_input.readlines()
+        card_copies = {i + 1: 1 for i in range(len(cards))}
+        for index, card in enumerate(cards):
+            current_number_of_copies = card_copies[index + 1]
+            pipe_card = card.replace(":", "|")
+            winning_numbers = set(map(int, re.findall(r"\d+", pipe_card.split("|")[1])))
+            card_numbers = set(map(int, re.findall(r"\d+", pipe_card.split("|")[2])))
+            matches = len(winning_numbers.intersection(card_numbers))
+            if matches > 0:
+                for i in range(1, matches + 1):
+                    card_copies[index + 1 + i] += current_number_of_copies
+        return card_copies
 
 
-helpers.print_timed_results(day=2, solution_func=RENAME_FUNC)
+helpers.print_timed_results(day=2, solution_func=get_total_scratchcards)
