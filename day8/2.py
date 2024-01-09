938dc61a3e7649c25668e23aafc9c55689769565
andrew gorman <andrew.gorman@hydrogrid.eu>
2024-01-09 09:05:29 +0100

    Begin hacking at day8p2 - need regex skills

--- day8/2.py
+++ day8/2.py
@@ -1,17 +1,41 @@
 import os
-
+import re
 import helpers
 
 
-def RENAME_FUNC(file_path: os.path):
-    RENAME = _RENAME_FUNC(file=file_path)
-    pass
+def count_moves_to_navigate_wasteland(file_path: os.path) -> int:
+    directional_instructions, network_look_up = _parse_instructions(file=file_path)
+    current_string_pattern = r"*\A$"
+    final_string_pattern = r"*\Z$"
+    number_of_directional_instructions = len(directional_instructions)
+    i = 0
+    while current_string != final_string:
+        if i >= number_of_directional_instructions:
+            direction = directional_instructions[i % number_of_directional_instructions]
+        else:
+            direction = directional_instructions[i]
+        direction = int(direction)
+        current_string = network_look_up[current_string][direction]
+        i += 1
+    return i
 
 
-def _RENAME_FUNC(file: os.path):
+def _parse_instructions(file: os.path) -> tuple[str, dict[str, tuple[str, str]]]:
     with open(file) as puzzle_input:
-        lines = puzzle_input.read()
-        print(lines)
+        directional_instructions = puzzle_input.readline()
+        network_look_up = dict()
+        for line in puzzle_input.readlines()[1:]:
+            line = line.strip().replace("(", "").replace(")", "")
+            network_look_up[line.split(" = ")[0].strip()] = (
+                line.split(" = ")[1].split(",")[0].strip(),
+                line.split(" = ")[1].split(",")[1].strip(),
+            )
+        directional_instructions = directional_instructions.replace("L", "0").replace(
+            "R", "1"
+        )
+    return directional_instructions.strip(), network_look_up
 
 
-helpers.print_timed_results(solution_func=RENAME_FUNC)
+helpers.print_timed_results(
+    solution_func=count_moves_to_navigate_wasteland, test_path_extension="eg2.txt"
+)
